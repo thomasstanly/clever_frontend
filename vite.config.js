3962fb4da118e525b653c80f5ffa351961fffc97
thomasstanly <thomasstanly12@gmail.com>
2024-09-07 01:24:04 +0530

    third

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-big-calendar/lib/css/react-big-calendar.css',
-        'moment',
-      ],
-    },
-  },
 })
